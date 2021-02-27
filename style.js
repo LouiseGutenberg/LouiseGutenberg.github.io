c61be52c3a2a7897e5d16bf9f0c75fd8143f2e83
LouiseGutenberg <74434270+LouiseGutenberg@users.noreply.github.com>
2021-02-27 10:54:44 -0700

    Update style.js
    nevermind

--- style.js
+++ style.js
@@ -18,29 +18,3 @@ for (i = 0; i < coll.length; i++) {
 
 /* contact.html */
 
-function flipcard () {
-  let flip = document.getElementById("business-card");
-  if (flip.innerHTML === <div class="row align-items-center justify-content-center">
-							<div class="col-6">
-								<img class="business-card-photo align-items-center" src="IMG_5791.jpg" alt="Photo of Louise Gutenberg">
-							</div>
-							<div class="col-6">
-								<p class="h5 text-center">Louise Gutenberg</p>
-								<p class="text-styling text-center">386.984.1083<br><span class="font-italic">louise.gutenberg@me.com</span></p>
-							</div> 
-
-						</div>) {
-    flip.innerHTML = "Swapped text!";
-  } else {
-    flip.innerHTML = <div class="row align-items-center justify-content-center">
-							<div class="col-6">
-								<img class="business-card-photo align-items-center" src="IMG_5791.jpg" alt="Photo of Louise Gutenberg">
-							</div>
-							<div class="col-6">
-								<p class="h5 text-center">Louise Gutenberg</p>
-								<p class="text-styling text-center">386.984.1083<br><span class="font-italic">louise.gutenberg@me.com</span></p>
-							</div> 
-
-						</div>;
-  }
-}

6317a25c088ace74182c65cb2b8cb328055fb110
LouiseGutenberg <74434270+LouiseGutenberg@users.noreply.github.com>
2021-02-27 10:35:11 -0700

    Update style.js
    lets try this new flipcard function!

--- style.js
+++ style.js
@@ -1,3 +1,5 @@
+/* resume.html */
+
 let coll = document.getElementsByClassName("collapsible");
 let i;
 
@@ -11,4 +13,34 @@ for (i = 0; i < coll.length; i++) {
       content.style.maxHeight = content.scrollHeight + "px";
     } 
   });
+}
+
+
+/* contact.html */
+
+function flipcard () {
+  let flip = document.getElementById("business-card");
+  if (flip.innerHTML === <div class="row align-items-center justify-content-center">
+							<div class="col-6">
+								<img class="business-card-photo align-items-center" src="IMG_5791.jpg" alt="Photo of Louise Gutenberg">
+							</div>
+							<div class="col-6">
+								<p class="h5 text-center">Louise Gutenberg</p>
+								<p class="text-styling text-center">386.984.1083<br><span class="font-italic">louise.gutenberg@me.com</span></p>
+							</div> 
+
+						</div>) {
+    flip.innerHTML = "Swapped text!";
+  } else {
+    flip.innerHTML = <div class="row align-items-center justify-content-center">
+							<div class="col-6">
+								<img class="business-card-photo align-items-center" src="IMG_5791.jpg" alt="Photo of Louise Gutenberg">
+							</div>
+							<div class="col-6">
+								<p class="h5 text-center">Louise Gutenberg</p>
+								<p class="text-styling text-center">386.984.1083<br><span class="font-italic">louise.gutenberg@me.com</span></p>
+							</div> 
+
+						</div>;
+  }
 }
